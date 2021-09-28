78306a649955c3459ad57aa9f055199e6d544aa5
Kateryna Bondarenko <katherine.bond74@gmail.com>
2021-09-28 18:00:34 +0300

    start

--- src/js/modules/dropdown.js
+++ src/js/modules/dropdown.js
@@ -1,18 +1,11 @@
  //dropdown
 
- function dropdown() {
-    const drop = document.querySelector(".dropdown__activator");
-    if(drop) {
-    drop.addEventListener("click", function () {
-      drop.classList.toggle("dropdown_open");
-    });
+function dropdown() {
+  const drop = document.querySelector(".dropdown__activator");
+  if(drop) {
+  drop.addEventListener("click", function () {
+    drop.classList.toggle("dropdown_open");
+  });
 
-    } 
-  }
-  
- 
-    dropdown();
- 
-  //dropdown
-
-  console.log(this)
+  } 
+}

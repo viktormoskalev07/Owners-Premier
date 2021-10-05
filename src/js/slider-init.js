85725384a596dc82025c2c0bde2f958d4801b552
Kate Bondarenko <seobootemarket1@gmail.com>
2021-10-05 17:48:50 +0300

    why choose + endless holiday + arrange a visit

--- src/js/slider-init.js
+++ src/js/slider-init.js
@@ -1,4 +1,4 @@
-let swiper = new Swiper(".HeroSwiper", {
+let HeroSwiper = new Swiper(".HeroSwiper", {
     slidesPerView: 1,
     spaceBetween: 0,
     loop:true,
@@ -7,4 +7,32 @@ let swiper = new Swiper(".HeroSwiper", {
         nextEl: '.hero-next',
         prevEl: '.hero-prev',
       }
-  });
+});
+let ChooseSwiper = new Swiper(".ChooseSwiper", {
+    spaceBetween: 0,
+    loop: true,
+    grabCursor:'true',
+    centeredSlides: false,
+    navigation: {
+        nextEl: '.choose-next',
+        prevEl: '.choose-prev',
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 3,
+        },
+        576: {
+            slidesPerView: 1.8,
+          
+        },
+        768: {
+            slidesPerView: 2.5,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+        1440: {
+          slidesPerView: 6,
+        },
+    },
+});

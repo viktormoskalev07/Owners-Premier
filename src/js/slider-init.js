642297131aea13ec0989044dcf20a70f33e2dd42
denis <denis@MacForMike.local>
2021-10-06 18:54:24 +0300

    home-details

--- src/js/slider-init.js
+++ src/js/slider-init.js
@@ -8,3 +8,21 @@ let swiper = new Swiper(".HeroSwiper", {
         prevEl: '.hero-prev',
       }
   });
+let bottomSlider = new Swiper('.bottom_slider', {
+    slidesPerView: 4,
+    autoHeight: true,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.bottom-next',
+        prevEl: '.bottom-prev',
+    }
+});
+let detailSlider = new Swiper('.detail_slider', {
+    slidesPerView: 1,
+    autoHeight: true,
+    spaceBetween: 1,
+    navigation: {
+        nextEl: '.detail-next',
+        prevEl: '.detail-prev',
+    }
+});
